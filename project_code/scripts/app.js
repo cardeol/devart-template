7a83fc10027c761368aa43acce38779a8a4becb9
Carlos De Oliveira <cardeol@gmail.com>
2014-03-22 20:21:55 +0000

    Cleaning

--- project_code/scripts/app.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('app',['firebase']);
-
-
-
-myApp.controller('mainController',function($scope, $firebase) {
- 	var ref = new Firebase("https://cardeoldata.firebaseio.com/");
-	$scope.fdata = $firebase(ref);	
-});
- 
